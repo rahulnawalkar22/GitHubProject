797d4499cd39fc6c97c3b20e24aa59ab04ca8c2c
dell <dell@DESKTOP-NM62KD2>
2024-09-24 12:37:58 +0530

    [snehadip] : added new project

---
+++ src/Sample_1/Demo_1.java
@@ -0,0 +1,17 @@
+package Sample_1;
+
+public class Demo_1 
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("Hi");
+		
+		System.out.println("Hi");
+		
+		System.out.println("Hello");
+		
+		System.out.println("Hello");
+		
+	}
+}
